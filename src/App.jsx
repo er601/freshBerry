ba62696cc53f83836c689204ff2f3d33b8960a84
er601 <erbolmedetbekov22@gmail.com>
2024-02-09 00:49:11 -0500

    created a project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.scss'
+import Header from "./components/Header/Header.jsx";
+
+function App() {
+  return (
+    <>
+        <Header />
+    </>
+  )
+}
+
+export default App
